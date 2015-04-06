9060e368c9dfd275b1beaa2e889040859752b7d9
Adnane <ad.gasmi@gmail.com>
2015-04-06 17:06:09 -0400

    Finish rater signup

--- app/controllers/raters_controller.rb
+++ app/controllers/raters_controller.rb
@@ -1,4 +1,27 @@
 class RatersController < ApplicationController
-  def new
+
+def show
+    @rater = Rater.find(params[:id])
   end
+
+  def new
+  	@rater = Rater.new
+  end
+
+    def create
+    @rater = Rater.new(rater_params)
+    if @rater.save
+    	flash[:success] = "Welcome to Ottawa's Spoon"
+      	redirect_to @rater
+    else
+      render 'new'
+    end
+  end
+
+
+    def rater_params
+      params.require(:rater).permit(:name, :email, :atype, :password,
+                                   :password_confirmation)
+    end
+
 end
--- app/models/rater.rb
+++ app/models/rater.rb
@@ -2,8 +2,8 @@ class Rater < ActiveRecord::Base
  	before_save { self.email = email.downcase }
 	validates :name, presence: true
 	validates :email, presence: true
-	validates :type, presence: true
-	validates :reputation, presence: true
+	validates :atype, presence: true
+
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
